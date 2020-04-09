4a0d42be1145895c0ba9c7b3082c942820e1ad1e
Alessio Coser <alessio.coser@xpeppers.com>
2020-04-09 09:31:42 +0200

    created abstract DaikonServer

---
+++ src/main/kotlin/daikon/core/ServerContext.kt
@@ -0,0 +1,14 @@
+package daikon.core
+
+class ServerContext: Context {
+    private val attributes = mutableMapOf<String, Any>()
+
+    override fun addAttribute(key: String, value: Any) {
+        attributes[key] = value
+    }
+
+    override fun <T> getAttribute(key: String): T {
+        @Suppress("UNCHECKED_CAST")
+        return attributes[key] as T
+    }
+}
