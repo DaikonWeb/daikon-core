95050c146ec5c4915a7d45f812106eb29201f567
Alessio Coser <alessio.coser@xpeppers.com>
2020-08-06 17:31:27 +0200

    permit to use before api with RouteAction

--- src/main/kotlin/daikon/core/DaikonServer.kt
+++ src/main/kotlin/daikon/core/DaikonServer.kt
@@ -30,76 +30,52 @@ abstract class DaikonServer(initializeActions: DaikonServer.() -> Unit = {}) : A
         stop()
     }
 
-    fun exception(exception: Class<out Throwable>, action: (Request, Response, Context, Throwable) -> Unit)
-            = exception(exception, ContextExceptionAction(action))
-
-    fun exception(exception: Class<out Throwable>, action: (Request, Response, Throwable) -> Unit)
-            = exception(exception, DummyExceptionAction(action))
-
+    fun exception(exception: Class<out Throwable>, action: (Request, Response, Context, Throwable) -> Unit) = exception(exception, ContextExceptionAction(action))
+    fun exception(exception: Class<out Throwable>, action: (Request, Response, Throwable) -> Unit) = exception(exception, DummyExceptionAction(action))
     fun exception(exception: Class<out Throwable>, action: ExceptionAction): DaikonServer {
         exceptions.add(ExceptionRoute(exception, action))
         return this
     }
 
     fun get(path: String, action: (Request, Response) -> Unit) = get(path, DummyRouteAction(action))
-
     fun get(path: String, action: (Request, Response, Context) -> Unit) = get(path, ContextRouteAction(action))
-
     fun get(path: String, action: RouteAction) = add(Method.GET, path, action)
 
     fun post(path: String, action: (Request, Response) -> Unit) = post(path, DummyRouteAction(action))
-
     fun post(path: String, action: (Request, Response, Context) -> Unit) = post(path, ContextRouteAction(action))
-
     fun post(path: String, action: RouteAction) = add(Method.POST, path, action)
 
     fun put(path: String, action: (Request, Response) -> Unit) = put(path, DummyRouteAction(action))
-
     fun put(path: String, action: (Request, Response, Context) -> Unit) = put(path, ContextRouteAction(action))
-
     fun put(path: String, action: RouteAction) = add(Method.PUT, path, action)
 
     fun delete(path: String, action: (Request, Response) -> Unit) = delete(path, DummyRouteAction(action))
-
     fun delete(path: String, action: (Request, Response, Context) -> Unit) = delete(path, ContextRouteAction(action))
-
     fun delete(path: String, action: RouteAction) = add(Method.DELETE, path, action)
 
     fun options(path: String, action: (Request, Response) -> Unit) = options(path, DummyRouteAction(action))
-
     fun options(path: String, action: (Request, Response, Context) -> Unit) = options(path, ContextRouteAction(action))
-
     fun options(path: String, action: RouteAction) = add(Method.OPTIONS, path, action)
 
     fun head(path: String, action: (Request, Response) -> Unit) = head(path, DummyRouteAction(action))
-
     fun head(path: String, action: (Request, Response, Context) -> Unit) = head(path, ContextRouteAction(action))
-
     fun head(path: String, action: RouteAction) = add(Method.HEAD, path, action)
 
     fun any(path: String, action: (Request, Response) -> Unit) = any(path, DummyRouteAction(action))
-
     fun any(path: String, action: (Request, Response, Context) -> Unit) = any(path, ContextRouteAction(action))
-
     fun any(path: String, action: RouteAction) = add(Method.ANY, path, action)
 
-    fun before(path: String = "/*", action: (Request, Response) -> Unit): DaikonServer {
-        beforeActions.add(Route(Method.ANY, joinPaths(path), DummyRouteAction(action)))
+    fun before(path: String = "/*", action: (Request, Response) -> Unit) = before(path, DummyRouteAction(action))
+    fun before(path: String = "/*", action: (Request, Response, Context) -> Unit) = before(path, ContextRouteAction(action))
+    fun before(path: String = "/*", action: RouteAction): DaikonServer {
+        beforeActions.add(Route(Method.ANY, joinPaths(path), action))
         return this
     }
 
-    fun before(path: String = "/*", action: (Request, Response, Context) -> Unit): DaikonServer {
-        beforeActions.add(Route(Method.ANY, joinPaths(path), ContextRouteAction(action)))
-        return this
-    }
-
-    fun after(path: String = "/*", action: (Request, Response) -> Unit): DaikonServer {
-        afterActions.add(Route(Method.ANY, joinPaths(path), DummyRouteAction(action)))
-        return this
-    }
-
-    fun after(path: String = "/*", action: (Request, Response, Context) -> Unit): DaikonServer {
-        afterActions.add(Route(Method.ANY, joinPaths(path), ContextRouteAction(action)))
+    fun after(path: String = "/*", action: (Request, Response) -> Unit) = after(path, DummyRouteAction(action))
+    fun after(path: String = "/*", action: (Request, Response, Context) -> Unit) = after(path, ContextRouteAction(action))
+    fun after(path: String = "/*", action: RouteAction): DaikonServer {
+        afterActions.add(Route(Method.ANY, joinPaths(path), action))
         return this
     }
 
